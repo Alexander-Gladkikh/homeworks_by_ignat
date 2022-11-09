db50168f0b348907afba2b1fb52e13ffcbd2aaf4
Alexander <som17son25@gmail.com>
2022-11-09 18:55:10 +0300

    hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+
+             const result = [...state].sort((a:UserType, b:UserType) => {
+                    if (a.name < b.name) return -1
+                    else if (a.name > b.name) return 1
+                    else return 0
+                })
+
+            return action.payload === 'up' ? result : result.reverse()
+
 
             return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+
+            return state.filter(el => el.age > action.payload) // need to fix
         }
         default:
             return state
     }
 }
+
+
