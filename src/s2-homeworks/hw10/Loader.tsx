94788c475d5a5b4d35704f5f4e9f6eb4256879ce
Alexander <som17son25@gmail.com>
2022-11-11 21:39:07 +0300

    hw10 completed styling

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,11 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+
+    <div className={s.loadingioSpinnerEclipseOtfo73gpen7}>
+        <div className={s.ldioBmgr8c4yoni}>
+            <div></div>
+        </div>
+    </div>
+
+</div>
