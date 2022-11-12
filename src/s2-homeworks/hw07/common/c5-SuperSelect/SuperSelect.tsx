2c5f73f08bd117120d5951ff7ef05f7ee6908a63
Alexander <som17son25@gmail.com>
2022-11-12 22:37:15 +0300

    hw12 completed

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -13,6 +13,7 @@ type DefaultSelectPropsType = DetailedHTMLProps<
 type SuperSelectPropsType = DefaultSelectPropsType & {
     options?: any[]
     onChangeOption?: (option: any) => void
+    onChange?: any
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
